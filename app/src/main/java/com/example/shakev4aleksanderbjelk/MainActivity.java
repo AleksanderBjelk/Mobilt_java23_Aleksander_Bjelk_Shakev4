5d717d6bc6c648eb2ad0e35b89286a44199f45c4
AleksanderBjelk <bjelkaleksander@gmail.com>
2024-08-26 15:14:17 +0200

    Får gyrskopet att visa sina värden

--- app/src/main/java/com/example/shakev4aleksanderbjelk/MainActivity.java
+++ app/src/main/java/com/example/shakev4aleksanderbjelk/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.shakev4aleksanderbjelk;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,20 +15,62 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    TextView textX , textY ,textZ ;
+    SensorManager sensorManager;
+    Sensor sensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        textX = findViewById(R.id.textX);
+        textY = findViewById(R.id.textY);
+        textZ = findViewById(R.id.textZ);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                    Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 
 
 
             return insets;
         });
+    }
+
+
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float x = sensorEvent.values[0]*10;
+        Log.i("hh", x+"");
+        float y = sensorEvent.values[1]*10;
+        float z= sensorEvent.values[2]*10;
+        textX.setText("X : " + (int) x + "rad s/m");
+        textY.setText("Y : " + (int) y + "rad s/m");
+        textZ.setText("Z : " + (int) z + "rad s/m");
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+        @Override
+    public void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        sensorManager.unregisterListener(this, sensor);
     }
 }
